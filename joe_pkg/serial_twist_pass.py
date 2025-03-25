5e91a9dda739d490907128e2b5c9b4ac68dde023
Divine Nduka <dndukac2007@gmail.com>
2025-03-25 17:52:29 -0400

    Fixed bugs in serial_twist_publisher

--- joe_pkg/serial_twist_pass.py
+++ joe_pkg/serial_twist_pass.py
@@ -1,11 +1,7 @@
-import time
-
 import rclpy
 import serial
 from geometry_msgs.msg import Twist
-from rclpy import Node
-
-ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)  # change serial name to arduino
+from rclpy.node import Node
 
 
 # define the subscriber node class to subscriber from twist_pub
@@ -17,12 +13,13 @@ class SerialTwistPass(Node):
             Twist, "desired_twist", self.serial_parse, 10
         )
         self.subscription  # prevent unused variable warning
+        self.ser = serial.Serial(
+            "/dev/ttyACM0", 9600, timeout=1
+        )  # change serial name to arduino
 
     # define the subscriber callback to receive data, parse it into the desired format, and send it
     # to the arduino over serial
     def serial_parse(self, msg):
-        global ser
-
         # parse the data for serial into the proper format
         lin_speed = str(msg.linear.x)
         ang_speed = str(msg.angular.z)
@@ -32,21 +29,27 @@ class SerialTwistPass(Node):
         # record the received message on the console
         self.get_logger().info(motor_command)
 
-        ser.reset_input_buffer()  # flush input buffer, discarding all its contents
-        ser.write(motor_command.encode("utf-8"))
+        self.ser.reset_input_buffer()  # flush input buffer, discarding all its contents
+        self.ser.write(motor_command.encode("utf-8"))
 
         # send the data to the arduino over serial
+
+    def destroy(self):
+        self.ser.close()  #  Close serial port before shutdown
+        super().destroy_node()
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    serial_twist_pass = SerialTwistPass
+    serial_twist_pass = SerialTwistPass()
 
-    rclpy.spin(serial_twist_pass)
+    try:
+        rclpy.spin(serial_twist_pass)
+    except KeyboardInterrupt:
+        pass  # Allow graceful shutdown with Ctrl+C
 
-    serial_twist_pass.destroy_node()
-    rclpy.shutdown()
+    serial_twist_pass.destroy()  # Call custom destroy method
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import find_packages, setup
 
-package_name = 'joe_pkg'
+package_name = "joe_pkg"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='divine',
-    maintainer_email='dndukac2007@gmail.com',
-    description='Functionality for JOE: MREN 203',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="divine",
+    maintainer_email="dndukac2007@gmail.com",
+    description="Functionality for JOE: MREN 203",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        	'key_twist_publisher = joe_pkg.twist_pub:main'
+        "console_scripts": [
+            "key_twist_publisher = joe_pkg.twist_pub:main",
+            "serial_twist_pass = joe_pkg.serial_twist_pass:main",
         ],
     },
 )
