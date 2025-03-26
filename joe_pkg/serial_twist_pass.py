a0a1f571323d538e86cba0f6f0a8a834423420f0
Divstar07 <123ndukachijindu@gmail.com>
2025-03-26 15:07:40 -0400

    Remove arduino talking back, keeping time delay in serial comm

--- joe_pkg/serial_twist_pass.py
+++ joe_pkg/serial_twist_pass.py
@@ -28,12 +28,12 @@ class SerialTwistPass(Node):
         motor_command = lin_speed + ":" + ang_speed + "\n"
 
         # record the received message on the console
-        # self.get_logger().info(motor_command)
+        self.get_logger().info(motor_command)
 
         self.ser.reset_input_buffer()  # flush input buffer, discarding all its contents
         self.ser.write(motor_command.encode("utf-8"))
-        line = self.ser.readline().decode('utf-8').rstrip()
-        self.get_logger().info(line)
+        # line = self.ser.readline().decode('utf-8').rstrip()
+        # self.get_logger().info(line)
         time.sleep(1)
 
         # send the data to the arduino over serial
